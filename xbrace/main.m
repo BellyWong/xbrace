0405aa3b562481234b34dbeca6411309a3ab1f3b
Aral Balkan <aral@aralbalkan.com>
2013-02-19 21:04:15 +0000

    Initial Commit

---
+++ xbrace/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xbrace
+//
+//  Created by Aral Balkan on 19/02/2013.
+//  Copyright (c) 2013 Aral Balkan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
