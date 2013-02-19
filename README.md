e7f717f707e88b53920505c249181ef2e79388a8
Aral Balkan <aral@aralbalkan.com>
2013-02-19 23:17:32 +0000

    Initial add.

---
+++ README.md
@@ -0,0 +1,24 @@
+Xbrace — get your braces on the next line in Xcode
+==================================================
+
+I was teaching my [Modern iOS Development workshop](http://moderniosdevelopment.com) today when Jamie, one of my students, asked if there was an easy way to get Xcode to put your braces on the next line in the built‐in system code snippets. I knew there used to be an easy way but that disappeared with Xcode 4. Googling it tonight, I found [this post by Doug Stephen](http://canadian-fury.com/2012/05/16/xcode-4-dot-3-place-all-autocompleted-opening-curly-braces-on-new-lines/) and [this StackOverflow thread](http://stackoverflow.com/questions/5120343/xcode-4-with-opening-brace-on-new-line) that suggested hacking the System Code Snippets file. The problem is that you have to do it anew with every version of Xcode. Who has the time? So, instead, I decided to whip up a quick command line app to automate the process.
+
+I’ve tested this with Xcode 4.6 and it appears to work.
+
+Usage:
+------
+
+  1. Download xbrace and copy it to /usr/local/bin or somewhere else that’s cosy (and on your path)
+  2. ```xbrace```
+
+That’s it!
+
+A note on backups:
+------
+
+Xbrace will back up your snippets file to your home folder (~). It will not overwrite existing backups so you cannot accidentally overwrite the backup by running it multiple times (although there is no reason to do that, of course).
+
+Limitations
+------------
+
+Use at your own risk. I haven’t tested this thoroughly. Also, when a new version of Xcode comes out, you need to manually delete the old backup to have new snippets file backed up. I’ll try and think of a more elegant way of handling this.
--- xbrace/main.m
+++ xbrace/main.m
@@ -2,20 +2,109 @@
 //  main.m
 //  xbrace
 //
+//  Changes the system code snippets for Xcode to place
+//  all autocompleted opening curly braces on new lines.
+//
+//  Uses the technique outlined by Doug Stephen at
+//  http://canadian-fury.com/2012/05/16/xcode-4-dot-3-place-all-autocompleted-opening-curly-braces-on-new-lines/
+//
+//  Also thanks to Leslie’s answer on StackOverflow for the path to the file
+//  http://stackoverflow.com/a/9638657/253485
+//
 //  Created by Aral Balkan on 19/02/2013.
-//  Copyright (c) 2013 Aral Balkan. All rights reserved.
+//  Copyright (c) 2013 Aral Balkan. Released under the MIT License.
 //
 
 #import <Foundation/Foundation.h>
 
+#define kSystemCodeSnippetsFilePath   @"/Applications/Xcode.app/Contents/PlugIns/IDECodeSnippetLibrary.ideplugin/Contents/Resources/SystemCodeSnippets.codesnippets"
+
+
+void showErrorMessage(NSError *error, NSString *errorMessage)
+{
+    NSLog(@"Error %@ your Xcode System Code Snippets file:\n%@", errorMessage, [error localizedDescription]);
+}
+
+
+NSString *replaceRegexpWithTemplateForString(NSString *pattern, NSString *template, NSString *string)
+{
+    NSError *error = nil;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionAnchorsMatchLines error:&error];
+    if (regex)
+    {
+        NSRange theWholeThing = NSMakeRange(0, [string length]);
+        
+        NSUInteger numberOfMatches = [regex numberOfMatchesInString:string options:0 range:theWholeThing];
+        NSLog(@"Number of matches: %lu", numberOfMatches);
+        
+        return [regex stringByReplacingMatchesInString:string options:0 range:theWholeThing withTemplate:template];
+    }
+    else
+    {
+        NSLog(@"Error in regular expression: %@\n%@", pattern, [error localizedDescription]);
+        return nil;
+    }
+}
+
+
+void backupTheSnippetsFileIfNecessary()
+{
+    
+    // The path to back up the SystemCodeSnippets file to.
+    NSString *snippetsBackupFilePath = [NSHomeDirectory() stringByAppendingString:@"/SystemCodeSnippets.codesnippets"];
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:snippetsBackupFilePath])
+    {
+        // A backup does not exist, make one.
+        NSLog(@"Backing up the Xcode System Code Snippets file…");
+
+        NSError *error = nil;
+        if ([[NSFileManager defaultManager] copyItemAtPath:kSystemCodeSnippetsFilePath toPath:snippetsBackupFilePath error:&error])
+        {
+            NSLog(@"…done.");
+        }
+        else
+        {
+            showErrorMessage(error, @"backing up");
+        }
+    }
+}
+
+
+void updateTheXcodeSystemCodeSnippetsFile()
+{
+    NSLog(@"Updating the Xcode System Code Snippets file…");
+    
+    NSError *error = nil;
+    NSString *systemCodeSnippets = [[NSString alloc] initWithContentsOfFile:kSystemCodeSnippetsFilePath encoding:NSUTF8StringEncoding error:&error];
+    if (systemCodeSnippets)
+    {
+        NSString *elsesEtc = replaceRegexpWithTemplateForString(@"\\} (.*?) \\{", @"\\}\n$1\n\\{", systemCodeSnippets);
+        
+        // Replace curly brackets on line ends with ones on the next line.
+        NSString *bracesOnNewLine = replaceRegexpWithTemplateForString(@"[ ]\\{", @"\n{", elsesEtc);
+        
+        // Note: 2 levels of indentation (8 spaces) is currently the max in the file; hardcoded for that.
+        NSString *fixedSecondLevelIndentation = replaceRegexpWithTemplateForString(@"^\\{\n        ", @"    {\n        ", bracesOnNewLine);
+        
+        NSString *fixedThatElse = replaceRegexpWithTemplateForString(@"^else\n    \\{", @"    else\n    \\{", fixedSecondLevelIndentation);
+        
+        NSString *finalVersion = fixedThatElse;
+        
+        NSLog(@"Final version: %@", finalVersion);
+    }
+    else
+    {
+        showErrorMessage(error, @"reading");
+    }    
+}
+
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        backupTheSnippetsFileIfNecessary();
+        updateTheXcodeSystemCodeSnippetsFile();
     }
     return 0;
 }
